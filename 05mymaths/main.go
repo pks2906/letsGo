b34513f6c84f952ffc1656b56f45229077eebca8
pks2906 <pratik1971sinha@gmail.com>
2024-10-08 13:31:59 +0530

    1k this year

---
+++ 05mymaths/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	//
+	"crypto/rand"
+	// "time"
+)
+
+func main() {
+	fmt.Println("Welcome to maths in golang")
+
+// 	// var mynumberOne int = 2
+// 	// var mynumbersTwo float64 = 4
+
+// 	//fmt.Println("The sum is:", mynumberOne+int(mynumbersTwo))
+
+	//random number
+	// rand.Seed(time.Now().UnixNano())
+	// fmt.Println(rand.Intn(5) + 1)
+
+	//random from crypto
+
+	myRandomNum, _ := rand.Int(rand.Reader, big.NewInt(5))
+	fmt.Println(myRandomNum)
+}
+
