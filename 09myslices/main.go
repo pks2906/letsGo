1c403afa8ac290b498af108b263940c664caaf85
pks2906 <pratik1971sinha@gmail.com>
2024-10-10 12:20:44 +0530

    remove a value from slices

--- 09myslices/main.go
+++ 09myslices/main.go
@@ -32,4 +32,12 @@ func main()  {
 	 sort.Ints(highScores)
 	 fmt.Println(highScores)
 	 fmt.Println(sort.IntsAreSorted(highScores))
+
+	 //how to remove a value from slices based on index
+
+	 var courses = []string{"React", "JS", "Python", "C++", "Ruby"}
+	 fmt.Println(courses)
+	 var index int = 2
+	 courses = append(courses[:index], courses[index+1:]...)
+	 fmt.Println(courses)
 }
