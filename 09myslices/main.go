ae86eed6d7695267f58fe7427eaa77928e9b3d28
pks2906 <pratik1971sinha@gmail.com>
2024-10-10 12:08:23 +0530

    slices in golang

---
+++ 09myslices/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main()  {
+	fmt.Println("Welcome to slices in Golang")
+
+	var fruitlist = []string{"Apple", "Mango", "Peach"}
+	fmt.Printf("Type of fruitlist is %T\n", fruitlist)
+
+	 fruitlist = append(fruitlist, "Tomato", "Banana")
+	 fmt.Println(fruitlist)
+
+	 fruitlist = append(fruitlist[1:])
+	 fmt.Println(fruitlist)
+
+	 highScores := make([]int, 4)
+
+	 highScores[0] = 234
+	 highScores[1] = 945
+	 highScores[2] = 465
+	 highScores[3] = 867
+	 //highScores[4] = 777
+
+	 highScores = append(highScores, 555, 666, 321)
+
+	 fmt.Println(highScores)
+
+	 sort.Ints(highScores)
+	 fmt.Println(highScores)
+	 fmt.Println(sort.IntsAreSorted(highScores))
+}
